c3f26b130bc30b6fedf3a0b97550856e426aa444
Bianca Boccato <biancab@motorola.com>
2019-03-09 19:29:39 -0300

    Add date picker to AddExpense activity

--- app/src/main/java/com/nanodegree/bianca/capstone/AddExpense.java
+++ app/src/main/java/com/nanodegree/bianca/capstone/AddExpense.java
@@ -1,8 +1,10 @@
 package com.nanodegree.bianca.capstone;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -19,4 +21,8 @@ public class AddExpense extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void showDatePicker(View view) {
+        DialogFragment dialogFragment = new DatePickerFragment();
+        dialogFragment.show(getSupportFragmentManager(), "date picker");
+    }
 }
