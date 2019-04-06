9a79743f531cc6839a0cb96a0db0453fa95901a6
Bianca Boccato <biancab@motorola.com>
2019-04-05 20:09:28 -0500

    Add back button to Budget Settings screen

--- app/src/main/java/com/nanodegree/bianca/capstone/BudgetSettings.java
+++ app/src/main/java/com/nanodegree/bianca/capstone/BudgetSettings.java
@@ -2,6 +2,7 @@ package com.nanodegree.bianca.capstone;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class BudgetSettings extends AppCompatActivity {
@@ -9,7 +10,9 @@ public class BudgetSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_budget_preferences);
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
