177bca6ff9ed379fd76c8065c1728edb6435df09
Bianca Boccato <biancab@motorola.com>
2019-04-13 18:43:39 -0300

    Add widget

--- app/src/main/java/com/nanodegree/bianca/capstone/Util.java
+++
@@ -1,7 +0,0 @@
-package com.nanodegree.bianca.capstone;
-
-public class Util {
-    public static String formatSummary(String label, float value) {
-        return String.format("%s US$ %.2f", label, value);
-    }
-}
