62674ebdfe89eb0e142790d05213401b38140c55
Bianca Boccato <biancab@motorola.com>
2019-04-21 10:46:49 -0300

    restore recycler view on orientation change

--- app/src/main/java/com/nanodegree/bianca/capstone/ExpensesDetails.java
+++ app/src/main/java/com/nanodegree/bianca/capstone/ExpensesDetails.java
@@ -4,12 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.nanodegree.bianca.capstone.data.Expense;
 import com.nanodegree.bianca.capstone.data.ExpenseDao;
@@ -19,11 +19,14 @@ import java.util.List;
 
 public class ExpensesDetails extends AppCompatActivity {
     private static final String TAG = "ExpensesDetails";
+    private static final String LIST_STATE_KEY = "list_state";
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private List<Expense> mExpenses;
     private ExpenseRoomDatabase mDb;
     private long expire;
+    private Parcelable mListState;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +43,9 @@ public class ExpensesDetails extends AppCompatActivity {
         new CurrentExpensesAsyncTask(this, mDb.expenseDao(), expire).execute();
 
         FloatingActionButton fab = findViewById(R.id.fab_add_expense);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AddExpense.class);
-                startActivity(intent);
-            }
+        fab.setOnClickListener(view -> {
+            Intent intent1 = new Intent(getApplicationContext(), AddExpense.class);
+            startActivity(intent1);
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -54,6 +54,22 @@ public class ExpensesDetails extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         new CurrentExpensesAsyncTask(this, mDb.expenseDao(), expire).execute();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putParcelable(LIST_STATE_KEY,
+                recyclerView.getLayoutManager().onSaveInstanceState());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+
+        // Retrieve list state and list/item positions
+        if (state != null)
+            mListState = state.getParcelable(LIST_STATE_KEY);
     }
 
     public void setExpenses(List<Expense> expenses) {
@@ -85,6 +101,9 @@ public class ExpensesDetails extends AppCompatActivity {
             mAdapter = new ExpenseAdapter(mExpenses);
             recyclerView.setAdapter(mAdapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+            if (mListState != null) {
+                recyclerView.getLayoutManager().onRestoreInstanceState(mListState);
+            }
         }
     }
 }
